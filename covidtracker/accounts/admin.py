010ca700b036b7f23d4c189a1eabfae981319b3d
AakashCode12 <aakashcoder1210@gmail.com>
2020-12-25 16:22:43 +0530

    Done till models

--- covidtracker/accounts/admin.py
+++ covidtracker/accounts/admin.py
@@ -1,3 +1,10 @@
+
 from django.contrib import admin
 
-# Register your models here.
+from .models import Patient, Doctor, Room, Result
+# # Register your models here.
+
+admin.site.register(Patient)
+admin.site.register(Doctor)
+admin.site.register(Room)
+admin.site.register(Result)
--- covidtracker/accounts/models.py
+++ covidtracker/accounts/models.py
@@ -3,39 +3,65 @@ from django.db import models
 # Create your models here.
 
 
-class Patient(models.Model):
-
-    name = models.TextField()
+class Patient (models.Model):
+    patient_id = models.AutoField(primary_key=True)
+    name = models.TextField(max_length=10)
     age = models.IntegerField()
     gender = models.CharField(max_length=10)  # M of F
-    phone = models.TextField()
-    address = models.TextField()
-    emailId = models.EmailField(max_length=254)
+    phone = models.TextField(max_length=1000)
+    address = models.TextField(max_length=1000)
+    email_id = models.EmailField(max_length=254)
     date = models.DateField(auto_now=False, auto_now_add=False)
-
     # Hospital
+    room_no = models.IntegerField(default=0)
+    floor_no = models.IntegerField(default=0)
+    o2_level = models.IntegerField(default=0)
+    temperature = models.IntegerField(default=97)
 
-    bedNo = models.IntegerField(default=0)
-    floor = models.IntegerField(default=0)
-    o2level = models.IntegerField(default=0)
+    doctorId = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.name
+
+
+class Room (models.Model):
+    floor_no = models.IntegerField(default=0)
+    room_no = models.IntegerField(default=0)
+    ventilator = models.BooleanField(default=False)
+    occupied = models.BooleanField(default=False)
+    patient_id = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.floorno+"-"+room_no
+
+
+class Doctor (models.Model):
+
+    doctor_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=500, default="")
+    phone = models.IntegerField(default=0)
+    experience = models.IntegerField(default=0)
+    # in years
+    no_of_patients = models.IntegerField(default=0)
+    no_of_recovered_patients = models.IntegerField(default=0)
+    no_of_decreased_patients = models.IntegerField(default=0)
+    no_of_patients_on_ventilator = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.doctor_id
+
+
+class Result (models.Model):
+    patient_id = models.IntegerField(default=0)
+    patient_name = models.CharField(default="", max_length=500)
     asymptomatic = models.BooleanField(default=False)
     cough = models.BooleanField(default=False)
     fever = models.BooleanField(default=False)
-    apetiteLoss = models.BooleanField(default=False)
-    temperature = models.IntegerField(default=97)
-    recovered = models.BooleanField(default=False)
-    admitted = models.BooleanField(default=True)
-    onVentilator = models.BooleanField(default=False)
-    decreased = models.BooleanField(default=False)
-    status = models.CharField(max_length=20)
-    # admitted , recovered , decreased, or on ventilator
+    apetite_loss = models.BooleanField(default=False)
+    # todo Here 3 denotes the no of test before (1 being the latest and 3 being the oldest)
+    covid_test_result_3 = models.BooleanField(default=True)
+    covid_test_result_2 = models.BooleanField(default=True)
+    covid_test_result_1 = models.BooleanField(default=True)
 
-    thirdastCOvidResult = models.BooleanField(default=True)
-    secondlastCovidResult = models.BooleanField(default=True)
-    lastCovidResult = models.BooleanField(
-        default=True)  # true for covid positive
-
-    # If(all three are false)
-
-    daysAdmitted = models.IntegerField(default=0)
-    doctorId = models.IntegerField(default=0)
+    def __str__(self):
+        return self.patient_name+" "+patient_id
